fdc86a348d8263d0083eb32f4ba10513a8d19b1b
cshi1 <128364086+cherryshi1@users.noreply.github.com>
2024-05-01 10:06:15 +0900

    springbootfile
    1

---
+++ FoodMateProject/src/main/java/com/domain/Com_Board.java
@@ -0,0 +1,36 @@
+package com.demo.domain;
+
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+
+import com.demo.dto.Com_Recipe;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@Entity
+@DynamicInsert 
+@DynamicUpdate
+public class Com_Board {
+
+		@Id
+		@OneToOne
+	    @JoinColumn(name="seq", nullable=false)
+		private Com_Board_Detail com_board_detail; //게시글번호
+		
+		@OneToOne
+		@JoinColumn(name="idx", nullable=false)
+		private Com_Recipe com_recipe; //레시피번호
+		
+		
+	}
