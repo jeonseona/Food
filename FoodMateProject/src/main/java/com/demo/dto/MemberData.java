3ff747b2ec4c57fd5c3fff83754fc9c9bb506fcd
jaecheon1 <158290204+jaecheon1@users.noreply.github.com>
2024-05-01 21:41:06 +0900

    Update MemberData.java

--- FoodMateProject/src/main/java/com/demo/dto/MemberData.java
+++ FoodMateProject/src/main/java/com/demo/dto/MemberData.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD:FoodMateProject/src/main/java/com/demo/dto/MemberData.java
 package com.demo.dto;
 
 import jakarta.persistence.Entity;
@@ -19,26 +18,3 @@ public class MemberData {
     private String email;
     private int nodata;
 }
-=======
-package com.demo.dto;
-
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import lombok.Data;
-
-@Data
-@Entity
-public class MemberData {
-    private String id;
-    private String password;
-    private int age;
-    private int height;
-    private int weight;
-    private String name;
-    private String gender;
-    private String email;
-   @Id
-    private int no_data;
-  
-}
->>>>>>> test_food:FoodMateProject/src/main/java/com/dto/MemberData.java
