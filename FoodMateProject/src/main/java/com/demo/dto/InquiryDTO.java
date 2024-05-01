9c0c7c3174d235294d9ddc7a7188659b67b8e4aa
jaecheon1 <french1022@naver.com>
2024-05-01 16:58:20 +0900

    .

--- FoodMateProject/src/main/java/com/demo/dto/InquiryDTO.java
+++ FoodMateProject/src/main/java/com/demo/dto/InquiryDTO.java
@@ -5,10 +5,14 @@ import lombok.Setter;
 
 import java.sql.Timestamp;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
 @Getter
 @Setter
+@Entity
 public class InquiryDTO {
-
+	@Id
     private Long inquiryId;
     private String name;
     private String email;
