6d39c8b599cafe4c3e55655bbc175c9ba2bf7047
jaecheon1 <158290204+jaecheon1@users.noreply.github.com>
2024-05-01 21:39:28 +0900

    Update Mypage.java

--- FoodMateProject/src/main/java/com/demo/domain/Mypage.java
+++ FoodMateProject/src/main/java/com/demo/domain/Mypage.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD:FoodMateProject/src/main/java/com/demo/domain/Mypage.java
 package com.demo.domain;
 
 import org.hibernate.annotations.DynamicInsert;
@@ -47,53 +46,3 @@ public class Mypage {
 	private Com_Board comBoard; // 커뮤니티게시글 (작성한 레시피목록 보기)
 
 }
-=======
-package com.demo.domain;
-
-import org.hibernate.annotations.DynamicInsert;
-import org.hibernate.annotations.DynamicUpdate;
-
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
-
-@Getter
-@Setter
-@Builder
-@ToString
-@Entity
-@NoArgsConstructor
-@AllArgsConstructor
-@DynamicInsert
-@DynamicUpdate
-public class Mypage {
-	@Id
-	private int bmi;	// bmi
-	private String height;	// 회원 키(소수점이하1자리)
-	private String weight;	// 회원 몸무게(소수점이하1자리)	
-	private String goal;	// 회원 목표(input radio를 이용해서 하나의 선택지만 선택)
-	
-	@Column(columnDefinition = "char(1) default 'y'")
-	private String food_preference;		// 음식선호도 : 'y'는 선호 | 'n'은 불호
-	
-	@Column(columnDefinition = "char(1) default 'n'")
-	private String allergy_food;		// 알레르기 음식 : 'n'은 없음 | 'y'는 있음
-	
-	@ManyToOne
-	@JoinColumn(name="id", nullable=false)
-	private Member member;		// 회원테이블 (개인정보 보기 및 수정)
-	
-	@ManyToOne
-	@JoinColumn(name = "seq", nullable = false)
-	private Com_Board comBoard; // 커뮤니티게시글 (작성한 레시피목록 보기)
-
-}
->>>>>>> test_food:FoodMateProject/src/main/java/com/domain/Mypage.java
