90d9a48e4623e0d843a51a104560068efcdfa20a
Limsujin <160580337+Limsujun@users.noreply.github.com>
2024-05-01 15:29:53 +0900

    Add files via upload

---
+++ FoodMateProject/src/main/java/com/dto/MemberData.java
@@ -0,0 +1,16 @@
+package com.demo.dto;
+
+import lombok.Data;
+
+@Data
+public class MemberData {
+    private String id;
+    private String password;
+    private int age;
+    private int height;
+    private int weight;
+    private String name;
+    private String gender;
+    private String email;
+    private int nodata;
+}
