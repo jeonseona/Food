eb4ddaceb0823040d3828d6cd722c4c856ffc95e
orin602 <tigers2589@naver.com>
2024-05-06 14:34:44 +0900

    test

--- FoodMateProject/src/main/java/com/demo/controller/MypageController.java
+++ FoodMateProject/src/main/java/com/demo/controller/MypageController.java
@@ -1,5 +1,7 @@
 package com.demo.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -7,63 +9,124 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.demo.domain.Member;
-import com.demo.service.MypageService;
+import com.demo.service.MemberService;
 
 import jakarta.servlet.http.HttpSession;
 
 @Controller
 public class MypageController {
 
-//	@Autowired
-//	private MemberService memberService;
 	@Autowired
-	private MypageService mypageService;
+	private MemberService memberService;
 	
-	// 마이페이지 메인
-	@GetMapping("/mypage")
-	public String getMypage(HttpSession session, Model model) {
-	    Member loginUser = (Member) session.getAttribute("loginUser"); // 세션에 저장된 로그인 유저
-
-	    if (loginUser == null) { // 비로그인 상태
-	        return "redirect:/login"; // 로그인 페이지로 리다이렉션
-	    } else { // 로그인 상태
-	        // 로그인 유저의 기타 정보
-	        model.addAttribute("loginUser", loginUser);
-	        
-	        // 음식 선호도 및 알레르기 정보도 로딩 
-//	        List<Food> foods = foodService.getFoodsPreferences(loginUser.getId());
-//	        model.addAttribute("foods", foods);
-	        
-	        // 사용자 ID로 검색된 레시피 목록 가져오기
-//	        List<Recipe> recipeList = recipeService.getRecipeList(loginUser.getId());
-//	        model.addAttribute("recipeList", recipeList);
-	        
-	        return "mypage/MypageMain"; // 마이페이지 메인 뷰 이름 반환
-	    }
+//	@Autowired
+//	private 커뮤니티게시판	// 내가 작성한 레시피용
+//	
+//	@Autowired
+//	private 추천기록	// 나의 추천받은 기록 목록용
+	
+	
+	// 마이페이지 메인화면
+	@GetMapping("/t_mypage")
+	public String mypageView(HttpSession session, Model model) {
+		Member loginUser = (Member)session.getAttribute("loginUser");
+		
+		if(loginUser == null) {
+			return "member/login";
+		} else {
+			// memberService.changeInfo(loginUser);
+			return "mypage/mypageMain";
+		}
 	}
 	
-	// 개인정보 수정처리
+	// 내 정보 화면
+	@GetMapping("/infoView")
+	public String myInfoView(HttpSession session, Model model, Member vo) {
+		Member loginUser = (Member)session.getAttribute("loginUser");
+		
+		if(loginUser == null) {
+			return "member/login";
+		} else {
+			String secrit = "********";
+			Member member = memberService.getMember(loginUser.getId());
+			model.addAttribute("loginUser", member);
+			model.addAttribute("********", secrit);
+		}
+		return "mypage/infoView";
+	}
+	
+	// 내 정보 수정 화면
+	@GetMapping("/infoUpdate")
+	public String infoUpdateView(HttpSession session, Model model) {
+		Member loginUser = (Member)session.getAttribute("loginUser");
+		
+		if(loginUser == null) {
+			return "member/login";
+		} else {
+			model.addAttribute("loginUser", loginUser);
+			return "mypage/infoUpdate";
+		}
+	}
+	
+	// 개인정보 수정하기
 	@PostMapping("/update_info")
-	public String updateInfo(Member vo) {
-		mypageService.changeInfo(vo);
-		return "mypage/MypageMain";
+	public String infoUpdateAction(HttpSession session, Member vo) {
+		Member loginUser = (Member)session.getAttribute("loginUser");
+		
+		if(loginUser == null) {
+			return "member/login";
+		} else {
+			
+			memberService.changeInfo(vo);
+			
+			return "mypage/mypageMain";
+		}
 	}
 	
-	// 바디데이터 수정처리
-	@PostMapping("/update_body")
-	public String updateBody(Member vo) {
-		mypageService.changeBody(vo);
-		return "mypage/MypageMain";
+	// 나의 호불호음식 및 알레르기 화면 ?????????????
+	@GetMapping("/preferences")
+	public String myFoodView(HttpSession session, Member vo) {
+		Member loginUser = (Member)session.getAttribute("loginUser");
+		
+		if(loginUser == null) {
+			return "member/login";
+		} else {
+			
+			return "t_mypage/xxxxxxxx";
+		}
 	}
 	
-	// 음식 선호도 및 알레르기
+	// 나의 레시피 화면
+	@GetMapping("/myRecipeList")
+	public String myRecipeListView(HttpSession session, Model model) {
+		Member loginUser = (Member)session.getAttribute("loginUser");
+		
+		if(loginUser == null) {
+			return "member/login";
+		} else {
+			List<> recipetList = Service.getRecipeList(loginUser.getId());
+			
+			model.addAttribute("recipeList", recipeList);
+			
+			return "t_mypage/MyRecipeList";
+		}
+	}
 	
+	// 추천받은 음식 화면
+	@GetMapping("/foodRecommend")
+	public String foodRecommendView(HttpSession session, Model model) {
+		Member loginUser = (Member)session.getAttribute("loginUser");
+		
+		if(loginUser == null) {
+			return "member/login";
+		} else {
+			List<> recommend = Service.getRecommendHistory(loginUser.getId());
+			
+			model.addAttribute("recommend", recommend);
+			
+			return "t_mypage/recommendHistory";
+		}
+	}
 	
-	
-	
-	
-	
-	
-	
-	
+
 }
--- FoodMateProject/src/main/java/com/demo/persistence/MemberRepository.java
+++ FoodMateProject/src/main/java/com/demo/persistence/MemberRepository.java
@@ -1,9 +1,45 @@
 package com.demo.persistence;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.demo.domain.Member;
+import com.demo.dto.MemberData;
+
+import jakarta.transaction.Transactional;
 
 public interface MemberRepository extends JpaRepository<Member, String> {
 
+	// 닉네임 중복체크
+	int countByNickname(String nickname);
+	
+	/**
+	 * 마이페이지용 테스트
+	 */
+	// 개인정보 변경 (id는 기본키라..)
+	// 이름, 비밀번호, 이메일, 키, 몸무게, BMI, 나이, 목표 (완)
+	@Transactional
+	@Modifying
+	@Query(value = "UPDATE member SET name=:name, pwd=:pwd, email=:email, height=:height, "
+			+ " weight=:weight, bmi=:bmi, age=:age, goal=:goal WHERE id=:id", nativeQuery = true)
+	public void changeInfo(@Param("id") String id, @Param("name") String name, @Param("pwd") String pwd, @Param("email") String email,
+					@Param("height") Integer height, @Param("weight") Integer weight, @Param("bmi") Double bmi, @Param("age") Integer age, @Param("goal") String goal);
+
+	// 사용자별 작성한 레시피 목록 조회
+	/*
+	 * @Query("SELECT recipe FROM ComBoardDetail recipe " +
+	 * " INNER JOIN Member m ON recipe.member.id = m.id " +
+	 * " WHERE recipe.member.id=?1 AND recipe.seq=?2") 
+	 * public 게시글테이블 getRecipeByMemberId(String id, int seq);
+	 */
+	
+	// 사용자별 추천받은 음식 목록
+	/*
+	 * @Query("SELECT history FROM RecommendHistory history " +
+	 * " INNER JOIN Member m ON history.member.id = m.id " +
+	 * " WHERE history.member.id=?1 ORDER BY history.recommend_date DESC") 
+	 * public 추천기록테이블 getRecommendHistoryByMemberId(String id);
+	 */
 }
