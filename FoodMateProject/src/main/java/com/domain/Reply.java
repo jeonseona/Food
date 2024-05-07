f478e5e3c8028fa26db08a0421a89fd217380ccb
cshi1 <128364086+cherryshi1@users.noreply.github.com>
2024-05-07 14:10:33 +0900

    Update Reply.java

--- FoodMateProject/src/main/java/com/domain/Reply.java
+++ FoodMateProject/src/main/java/com/domain/Reply.java
@@ -1,4 +1,7 @@
 package com.demo.domain;
+import java.util.Date;
+
+import org.hibernate.annotations.ColumnDefault;
 import org.hibernate.annotations.DynamicInsert;
 import org.hibernate.annotations.DynamicUpdate;
 
@@ -10,9 +13,11 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToOne;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
@@ -20,6 +25,7 @@ import lombok.ToString;
 @Setter
 @ToString
 @AllArgsConstructor
+@NoArgsConstructor
 @Entity
 @DynamicInsert 
 @DynamicUpdate
@@ -31,18 +37,17 @@ public class Reply {
 	
 	private String content;
 	
-	@OneToOne
+	@ManyToOne
     @JoinColumn(name="seq", nullable=false)
 	private Com_Board_Detail com_board_detail;
 	
 	@ManyToOne
     @JoinColumn(name="no_data", nullable=false)
 	private MemberData member_data;
-
 	
-	public Reply() {}
-	
-	
+	@Temporal(value=TemporalType.TIMESTAMP)
+    @ColumnDefault("sysdate")
+    private Date r_regdate;
 
 
 }
