6b387e2806625fc8d01075022b657d0e93bfd757
Limsujin <160580337+Limsujun@users.noreply.github.com>
2024-05-08 18:02:37 +0900

    FoodMATE1
    아직 오류사항 있습니다 ,,!!

---
+++ src/main/java/com/demo/persistence/MemberRepository.java
@@ -0,0 +1,33 @@
+package com.demo.persistence;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.demo.domain.Member;
+import com.demo.dto.MemberData;
+
+public interface MemberRepository extends JpaRepository<Member, String> {
+
+    @Modifying
+    @Transactional
+    // 비밀번호조회
+    Optional<Member> findByPwd(String pwd);
+    
+    // ID를 사용하여 회원 조회
+    Optional<Member> findById(String id);
+    
+    // 이름을 포함하는 회원 데이터 조회
+    List<MemberData> findByNameContaining(String name);
+
+    // 이름과 이메일을 사용하여 회원 조회
+    Member findByNameAndEmail(String name, String email);
+
+
+    Member findByIdAndNameAndEmail(String id, String name, String email);
+    
+}
