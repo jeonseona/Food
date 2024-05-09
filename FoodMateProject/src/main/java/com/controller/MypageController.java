23d65c1aa06eeb4e37a8e22fe36031b9881ac6a5
Orin602 <167276559+Orin602@users.noreply.github.com>
2024-05-09 10:21:44 +0900

    Update MypageController.java

--- FoodMateProject/src/main/java/com/controller/MypageController.java
+++ FoodMateProject/src/main/java/com/controller/MypageController.java
@@ -29,6 +29,13 @@ public class MypageController {
 	@Autowired
 	private Recommend_HistoryService rhService;
 
+	@GetMapping("/")
+	public String homePage(Model model) {
+		// 홈페이지에 필요한 데이터를 모델에 추가하고, 뷰 이름을 반환합니다.
+		model.addAttribute("welcomeMessage", "건강한 식단을 추천해드릴게요!");
+		return "main"; // 여기서 "main"는 타임리프 템플릿 파일의 이름입니다.
+	}
+		
 	// 마이페이지 메인화면
 	@GetMapping("/mypage")
 	public String mypageView(HttpSession session, Model model) {
