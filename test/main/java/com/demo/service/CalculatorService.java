0277ee182359f3c8d9786483ad2f111c73cc5922
cshi1 <128364086+cherryshi1@users.noreply.github.com>
2024-05-10 17:56:53 +0900

    d
    d

--- test/main/java/com/demo/service/CalculatorService.java
+++
@@ -1,9 +0,0 @@
-package com.demo.service;
-
-import com.demo.dto.CalculationResult;
-import com.demo.dto.MemberData;
-
-public interface CalculatorService {
-    CalculationResult calculate(MemberData memberData);
-    
-}
