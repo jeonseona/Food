2635915b77f6988f8a38ea291bb18201f0ced456
Orin602 <167276559+Orin602@users.noreply.github.com>
2024-05-10 09:56:56 +0900

    Update Recommend_HistoryRepository.java

--- FoodMateProject/src/main/java/com/persistence/Recommend_HistoryRepository.java
+++ FoodMateProject/src/main/java/com/persistence/Recommend_HistoryRepository.java
@@ -1,7 +1,7 @@
 package com.demo.persistence;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -11,10 +11,7 @@ import com.demo.domain.Recommend_History;
 public interface Recommend_HistoryRepository extends JpaRepository<Recommend_History, String> {
 
 	// 회원별 추천받은 음식(레시피)기록 조회
-	@Query(value= "SELECT rh FROM RecommendHistory rh "+
-			" JOIN MemberData m ON rh.h_no_data.id = m.id"+
-			" JOIN FoodRecipe r ON rh.recommend_food.idx = r.idx"+
-			" WHERE m.id=:id ORDER BY rh.recommend_date ASC", nativeQuery=false)
-	public Page<Recommend_History> getMyRecommendHistoryById(@Param("id") String id, Pageable pageable);
+	@Query("SELECT rh FROM RecommendHistory rh JOIN rh.member m WHERE m.id =:id")
+	public List<Recommend_History> getMyRecommendHistoryById(@Param("id") String id);
 	
 }
