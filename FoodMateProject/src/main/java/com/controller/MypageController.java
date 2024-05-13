ba08435865ad3a65f222be47e170eb0eabfa8da5
Orin602 <167276559+Orin602@users.noreply.github.com>
2024-05-13 14:15:45 +0900

    Update MypageController.java

--- FoodMateProject/src/main/java/com/controller/MypageController.java
+++ FoodMateProject/src/main/java/com/controller/MypageController.java
@@ -3,19 +3,17 @@ package com.demo.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import com.demo.domain.Com_Board_Detail;
-import com.demo.domain.Member;
+import com.demo.domain.MemberData;
 import com.demo.domain.Recommend_History;
-import com.demo.dto.MemberData;
+import com.demo.domain.askBoard;
 import com.demo.service.Com_Board_DetailService;
-import com.demo.service.MemberDataService;
+import com.demo.service.CustomerService;
 import com.demo.service.MemberService;
 import com.demo.service.Recommend_HistoryService;
 
@@ -25,16 +23,19 @@ import jakarta.servlet.http.HttpSession;
 public class MypageController {
 
 	@Autowired
-	private MemberDataService mdService;
+	private MemberService memberService;
 	@Autowired
-	private Com_Board_DetailService cbdService;
+	private Com_Board_DetailService boardService;
 	@Autowired
-	private Recommend_HistoryService rhService;
+	private Recommend_HistoryService recommendService;
+	@Autowired
+	private CustomerService customerService;
 
+	
 	// 마이페이지 메인화면
 	@GetMapping("/mypage")
 	public String mypageView(HttpSession session, Model model) {
-		Member loginUser = (Member)session.getAttribute("loginUser");
+		MemberData loginUser = (MemberData)session.getAttribute("loginUser");
 		
 		if(loginUser == null) {
 			return "redirect:/login";
@@ -47,15 +48,13 @@ public class MypageController {
 	// 내 정보 화면
 	@GetMapping("/infoView")
 	public String myInfoView(HttpSession session, Model model) {
-		Member loginUser = (Member)session.getAttribute("loginUser");
+		MemberData loginUser = (MemberData)session.getAttribute("loginUser");
 		
 		if(loginUser == null) {
 			return "redirect:/login";
 		} else {
-			// 회원 ID를 사용하여 MemberData 조회
-	        MemberData memberData = mdService.findByMember(loginUser);
-	        model.addAttribute("member", loginUser);  // Member 테이블 정보
-	        model.addAttribute("memberData", memberData);  // MemberData 테이블 정보
+			
+	        model.addAttribute("memberData", loginUser);
 			model.addAttribute("********", "********");
 		}
 		return "mypage/infoView";
@@ -64,7 +63,7 @@ public class MypageController {
 	// 내 정보 수정 화면
 	@GetMapping("/infoUpdate")
 	public String infoUpdateView(HttpSession session, Model model) {
-		Member loginUser = (Member)session.getAttribute("loginUser");
+		MemberData loginUser = (MemberData)session.getAttribute("loginUser");
 		
 		if(loginUser == null) {
 			return "redirect:/login";
@@ -74,44 +73,69 @@ public class MypageController {
 		}
 	}
 	
-	// 개인정보 수정하기
+	// 개인정보 수정
 	@PostMapping("/update_info")
 	public String infoUpdateAction(HttpSession session, MemberData vo) {
-		Member loginUser = (Member)session.getAttribute("loginUser");
+		MemberData loginUser = (MemberData)session.getAttribute("loginUser");
 		
 		if(loginUser == null) {
 			return "redirect:/login";
 		} else {
-			vo.setMember(loginUser);	// 로그인한 회원수정
-			mdService.changeInfo(vo);
+			// 로그인한 회원수정
+			memberService.changeInfo(vo);
 			
 			return "mypage/mypageMain";
 		}
 	}
 	
-	// 나의 호불호음식 및 알레르기 화면 ?????????????
-	@GetMapping("/preferences")
-	public String myFoodView(HttpSession session, MemberData vo) {
-		Member loginUser = (Member)session.getAttribute("loginUser");
+	// 닉네임 중복 확인 처리
+	@GetMapping("/nickname_check_form")
+	public String nicknameCheckView(MemberData vo, Model model) {
+		int result = memberService.confirmNickname(vo.getNickname());
+		
+		model.addAttribute("message", result);
+		model.addAttribute("nickname", vo.getNickname());
+		
+		return "mypage/nickcheck";
+	}
+	
+	// 바디데이터 수정 화면
+	@GetMapping("bodyUpdate")
+	public String bodyUpdateView(HttpSession session) {
+		MemberData loginUser = (MemberData)session.getAttribute("loginUser");
 		
 		if(loginUser == null) {
 			return "redirect:/login";
 		} else {
+			return "mypage/bodyUpdate";
+		}
+	}
+	
+	// 바디데이터 수정
+	@PostMapping("/update_body")
+	public String bodyUpdateAction(HttpSession session, MemberData vo) {
+		MemberData loginUser = (MemberData)session.getAttribute("loginUser");
+		
+		if(loginUser == null) {
+			return "redirect:/login";
+		} else {
+			// 로그인한 회원 바디데이터 수정
+			vo.setId(loginUser.getId());
+			memberService.changeBodyData(vo);
 			
-			return "mypage/preferences";
+			return "mypage/mypageMain";
 		}
 	}
 	
 	// 나의 레시피 화면
 	@GetMapping("/myRecipeList")
-	public String myRecipeListView(HttpSession session, Model model,
-			@RequestParam(defaultValue = "1") int page, @RequestParam(defaultValue = "5") int size) {
-		Member loginUser = (Member)session.getAttribute("loginUser");
+	public String myRecipeListView(HttpSession session, Model model) {
+		MemberData loginUser = (MemberData)session.getAttribute("loginUser");
 		
 		if(loginUser == null) {
 			return "redirect:/login";
 		} else {
-			List<Com_Board_Detail> recipeList = cbdService.getMyRecipe(loginUser.getId());
+			List<Com_Board_Detail> recipeList = boardService.getMyRecipe(loginUser.getId());
 			
 			model.addAttribute("recipeList", recipeList);
 			
@@ -121,18 +145,74 @@ public class MypageController {
 	
 	// 추천받은 음식 화면
 	@GetMapping("/foodRecommend")
-	public String foodRecommendView(HttpSession session, Model model,
-			@RequestParam(defaultValue = "1") int page, @RequestParam(defaultValue = "5") int size) {
-		Member loginUser = (Member)session.getAttribute("loginUser");
+	public String foodRecommendView(HttpSession session, Model model) {
+		MemberData loginUser = (MemberData)session.getAttribute("loginUser");
 		
 		if(loginUser == null) {
 			return "redirect:/login";
 		} else {
-			List<Recommend_History> recommend = rhService.getMyRecommendHistory(loginUser.getId());
+			List<Recommend_History> recommend = recommendService.getMyRecommendHistory(loginUser.getId());
 			model.addAttribute("recommend", recommend);
 			
 			return "mypage/recommendHistory";
 		}
 	}
+	
+	// 1:1문의 관련
+	@GetMapping("/myInquiry")
+	public String myInquiryView(HttpSession session, Model model) {
+		MemberData loginUser = (MemberData)session.getAttribute("loginUser");
+		
+		if(loginUser == null) {
+			return "redirect:/login";
+		} else {
+			List<askBoard> inquiry = customerService.getMyInquiry(loginUser.getId());
+			
+			model.addAttribute("InquiryList", inquiry);
+			return "mypage/myInquiry";
+		}
+	}
+	
+	
+	/* 추가 기능용 자리
+	 * 아직 구상중..
+	@PostMapping("/myGoal")
+	public String adviceOfGoal(HttpSession session, Model model) {
+		MemberData loginUser = (MemberData)session.getAttribute("loginUser");
+		
+		if(loginUser == null) {
+			return "redirect:login";
+		} else {
+			
+			
+			return "mypage/advice";
+		}
+	}
+	
+	
+	
+	
+	
+	
+	*/
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 
 }
