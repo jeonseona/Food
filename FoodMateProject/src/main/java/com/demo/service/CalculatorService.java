3b0dff2e39908f0d5ed7217e3caef0f459b1f228
cshi1 <128364086+cherryshi1@users.noreply.github.com>
2024-05-23 16:10:23 +0900

    통합본
    통합본

--- FoodMateProject/src/main/java/com/demo/service/CalculatorService.java
+++
@@ -1,9 +0,0 @@
-package com.demo.service;
-
-import com.demo.dto.CalculationResult;
-import com.demo.domain.MemberData;
-
-public interface CalculatorService {
-    CalculationResult calculate(MemberData memberData);
-    
-}
