c3f82ca46dc2538fc3262d728575b65e07fec0d7
jaecheon1 <158290204+jaecheon1@users.noreply.github.com>
2024-05-28 14:19:33 +0900

    Add files via upload

---
+++ 피클파일 자동업데이트/PythonScriptRunner.java
@@ -0,0 +1,26 @@
+package com.demo.service;
+
+import java.io.IOException;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class PythonScriptRunner {
+
+    public void runPythonScript() {
+        try {
+            // Python 스크립트 실행
+            Process process = Runtime.getRuntime().exec("python E:/Student/MachineLearning/autoUpdate.py");
+            int exitCode = process.waitFor();
+
+            // 실행 결과 처리
+            if (exitCode == 0) {
+                System.out.println("Python script executed successfully.");
+            } else {
+                System.err.println("Error: Python script execution failed.");
+            }
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
